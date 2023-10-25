f97cbe75e830dca664fb4dd3684e2b48d62c5b49
Lavish <64689132+lavish440@users.noreply.github.com>
2023-10-25 21:40:42 +0530

    Added slider snapping with ticks underneath

--- trig.js
+++ trig.js
@@ -1,9 +1,18 @@
+let val = [0, 1, 2, 3, 4, 5];
 var slider = document.getElementById("my-range");
 var output = document.getElementById("demo");
+
+function getClosest(arr, val) {
+  return arr.reduce(function (prev, curr) {
+    return Math.abs(curr - val) < Math.abs(prev - val) ? curr : prev;
+  });
+}
 
 output.innerHTML = slider.value;
 
 slider.oninput = function () {
+  let closest = getClosest(val, this.value);
+  this.value = closest;
   output.innerHTML = this.value;
   console.log(this.value);
 };
