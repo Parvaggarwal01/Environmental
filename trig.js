44f950a19abed76dc704c7684aa08f868ea12254
Lavish <64689132+lavish440@users.noreply.github.com>
2023-10-26 00:02:08 +0530

    Fixed a bug in the slider

--- trig.js
+++ trig.js
@@ -3,25 +3,25 @@ var slider = document.getElementById("my-range");
 var output = document.getElementById("demo");
 
 function getClosest(arr, val) {
-	return arr.reduce(function(prev, curr) {
-		return Math.abs(curr - val) < Math.abs(prev - val) ? curr : prev;
-	});
+  return arr.reduce(function (prev, curr) {
+    return Math.abs(curr - val) < Math.abs(prev - val) ? curr : prev;
+  });
 }
 
-output.innerHTML = slider.value;
+// output.innerHTML = slider.value;
 
-slider.oninput = function() {
-	let closest = getClosest(val, this.value);
-	this.value = closest;
-	output.innerHTML = this.value;
-	console.log(this.value);
-	if (closest == 0) {
-		var imageshown = "resources/images/jungle.jpg";
-	} else if (closest == 1) {
-		var imageshown = "resources/images/trees.jpg";
-	} else {
-		var imageshown = "resources/images/himalaya.jpg";
-	}
+slider.oninput = function () {
+  let closest = getClosest(val, this.value);
+  this.value = closest;
+  // output.innerHTML = this.value;
+  console.log(this.value);
+  if (closest == 0) {
+    var imageshown = "resources/images/jungle.jpg";
+  } else if (closest == 1) {
+    var imageshown = "resources/images/trees.jpg";
+  } else {
+    var imageshown = "resources/images/himalaya.jpg";
+  }
 
-	document.getElementById("img").src = imageshown;
+  document.getElementById("img").src = imageshown;
 };
